a2e480bebf2432bc20e2fcf33b1f682ca53f7a9a
“Jen <Jen.j.eyring@gmail.com>
2022-03-31 16:12:21 -0500

    added limite exercises

---
+++ limit_exercises.sql
@@ -0,0 +1,40 @@
+USE employees;
+
+SELECT DISTINCT title FROM titles;
+
+-- 2) List the first 10 distinct last name sorted in descending order.
+
+SELECT DISTINCT last_name
+FROM employees
+ORDER BY last_name LIMIT 10;
+
+
+-- 3) Find all previous or current employees hired in the 90s and born on Christmas. 
+
+SELECT *
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '____-12-25';
+
+-- Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records. 
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '____-12-25'
+ORDER BY hire_date LIMIT 5;
+
+-- Write a comment in your code that lists the five names of the employees returned.
+-- 'Alselm','Cappello','Utz','Mandell'
+-- 'Bouchung','Schreiter'
+-- 'Baocai','Kushner'
+-- 'Petter','Stroustrup'
+
+-- 4)Try to think of your results as batches, sets, or pages. 
+-- The first five results are your first page. 
+-- The five after that would be your second page, etc. 
+-- Update the query to find the tenth page of results.
+
+SELECT *
+FROM employees
+LIMIT 5 OFFSET 50;
