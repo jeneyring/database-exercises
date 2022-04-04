1701d1f0cf150e2e9e71812fc608958353fac480
“Jen <Jen.j.eyring@gmail.com>
2022-04-04 12:31:18 -0500

    more fixes

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,17 +9,18 @@ FROM users, roles;
 -- Before you run each query, guess the expected number of results.
 
 -- INNER JOIN:
-SELECT users.name AS user_name, roles.name AS role_name
+SELECT *
 FROM users
 JOIN roles on users.role_id=roles.id;
 
+
 -- LEFT JOIN:
-SELECT users.name AS user_name, roles.name AS role_name
+SELECT *
 FROM users
 LEFT JOIN roles on users.role_id=roles.id;
 
 -- RIGHT JOIN:
-SELECT users.name AS user_name, roles.name AS role_name
+SELECT *
 FROM users
 RIGHT JOIN roles on users.role_id=roles.id;
 
@@ -45,6 +46,16 @@ JOIN dept_manager AS manager
 JOIN departments 
   ON departments.dept_no = manager.dept_no;
   
+#OR....
+  
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS current_department_manager
+FROM employees AS e
+JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
+AND to_date > CURDATE()
+JOIN departments AS d USING (dept_no) #USING is shorthand version of ON...only if name is on both tables...ex. 
+#dept_no on both departments and dept_manager
+ORDER BY dept_name;
+  
   
 -- 3) Find the name of all departments currently managed by women.
 
@@ -55,6 +66,16 @@ JOIN dept_manager AS manager
 JOIN departments 
   ON departments.dept_no = manager.dept_no
   WHERE employees.gender = 'F';
+  
+#OR...
+SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS current_department_manager
+FROM employees AS e
+JOIN dept_manager AS dm ON e.emp_no = dm.emp_no
+AND to_date > CURDATE()
+AND gender = 'F'
+JOIN departments AS d USING (dept_no) #USING is shorthand version of ON...only if name is on both tables...ex. 
+#dept_no on both departments and dept_manager
+ORDER BY dept_name;
   
 -- 4) Find the current titles of employees currently working in the Customer Service department.
 
@@ -67,3 +88,35 @@ ON dept_emp.emp_no = title.emp_no
 WHERE title.to_date LIKE '9999%' AND dept_emp.to_date LIKE '9999%' AND dept_no = 'd009'
 GROUP BY title.title;
 
+#OR...
+
+SELECT t.title, COUNT(de.emp_no) AS Count
+FROM dept_emp AS de
+JOIN titles AS t ON de.emp_no = t.emp_no
+AND t.to_date > CURDATE()
+AND de.to_day > CURDATE()
+
+JOIN departments AS 
+
+-- 5) Find the current salary of all current managers.
+SELECT departments.dept_name, CONCAT(first_name, ' ', last_name) AS name, salaries.salary
+FROM employees 
+JOIN departments
+ON departments.dept_no = dept_manager.dept_no
+JOIN dept_manager
+
+-- 6) Find the number of current employees in each department.
+SELECT dept_emp.dept_no, departments.dept_name, SUM(emp.num_employees)
+FROM employees;
+
+-- 7) Which department has the highest average salary? Hint: Use current not historic information.
+
+-- 8) Who is the highest paid employee in the Marketing department?
+
+-- 9) Which current department manager has the highest salary?
+
+-- 10)Determine the average salary for each department. Use all salary information and round your results.
+
+-- 11) BONUS: Find the names of all current employees, their department name, and their current manager's name.
+
+-- 12) BONUS: Who is the highest paid employee within each department.
