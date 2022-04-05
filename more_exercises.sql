93df63588e782293b4ec93e70ff39bc993169207
“Jen <Jen.j.eyring@gmail.com>
2022-04-05 12:00:21 -0500

    updating more exercises

--- more_exercises.sql
+++ more_exercises.sql
@@ -39,4 +39,47 @@ WHERE payment_date >= '2005-05-27';
 
 SELECT payment_id, amount, payment_date
 FROM payment
-WHERE payment_date >= '2005-05-27';
+WHERE payment_date = '2005-05-27';
+
+-- Select all columns from the customer table for rows that have 
+-- last names beginning with S and first names ending with N.
+
+SELECT first_name, last_name
+FROM customer
+WHERE first_name LIKE '%N'
+	AND last_name LIKE 'S%';
+    
+-- Select all columns from the customer table for rows where 
+-- the customer is inactive or has a last name beginning with "M".
+
+SELECT * 
+FROM customer
+LIMIT 10;
+
+SELECT *
+FROM customer
+WHERE active = 0
+	OR last_name LIKE 'M%';
+    
+--  Select all columns from the category table for rows where the primary key 
+-- is greater than 4 and the name field begins with either C, S or T.
+
+SELECT *
+FROM category
+WHERE category_id > 4
+	AND category.name LIKE 'C%'
+		OR category.name LIKE 'S%' 
+        OR category.name LIKE 'T%';
+        
+-- Select all columns minus the password column from the staff table 
+-- for rows that contain a password.
+
+DESCRIBE staff;
+SELECT *, NULL AS
+FROM staff;
+
+
+
+USE category;
+DESCRIBE category;
+
